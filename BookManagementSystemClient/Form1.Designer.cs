52d8711263710f7cb2667fca43eb70569068b587
Aleller <1640799791@qq.com>
2018-05-04 10:48:20 +0800

    做好了登录界面

--- BookManagementSystemClient/Form1.Designer.cs
+++ BookManagementSystemClient/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BookManagementSystemClient
 {
-    partial class Form1
+    partial class Form_login
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_login = new System.Windows.Forms.Button();
+            this.button_register = new System.Windows.Forms.Button();
+            this.label_userName = new System.Windows.Forms.Label();
+            this.label_password = new System.Windows.Forms.Label();
+            this.textBox_userName = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_login
+            // 
+            this.button_login.Location = new System.Drawing.Point(52, 185);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(75, 23);
+            this.button_login.TabIndex = 0;
+            this.button_login.Text = "登录";
+            this.button_login.UseVisualStyleBackColor = true;
+            // 
+            // button_register
+            // 
+            this.button_register.Location = new System.Drawing.Point(172, 185);
+            this.button_register.Name = "button_register";
+            this.button_register.Size = new System.Drawing.Size(75, 23);
+            this.button_register.TabIndex = 1;
+            this.button_register.Text = "注册";
+            this.button_register.UseVisualStyleBackColor = true;
+            // 
+            // label_userName
+            // 
+            this.label_userName.AutoSize = true;
+            this.label_userName.Location = new System.Drawing.Point(27, 46);
+            this.label_userName.Name = "label_userName";
+            this.label_userName.Size = new System.Drawing.Size(53, 12);
+            this.label_userName.TabIndex = 2;
+            this.label_userName.Text = "用户名：";
+            // 
+            // label_password
+            // 
+            this.label_password.AutoSize = true;
+            this.label_password.Location = new System.Drawing.Point(29, 97);
+            this.label_password.Name = "label_password";
+            this.label_password.Size = new System.Drawing.Size(41, 12);
+            this.label_password.TabIndex = 3;
+            this.label_password.Text = "密码：";
+            // 
+            // textBox_userName
+            // 
+            this.textBox_userName.Location = new System.Drawing.Point(133, 43);
+            this.textBox_userName.Name = "textBox_userName";
+            this.textBox_userName.Size = new System.Drawing.Size(100, 21);
+            this.textBox_userName.TabIndex = 4;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(133, 87);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(100, 21);
+            this.textBox_password.TabIndex = 5;
+            // 
+            // Form_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBox_userName);
+            this.Controls.Add(this.label_password);
+            this.Controls.Add(this.label_userName);
+            this.Controls.Add(this.button_register);
+            this.Controls.Add(this.button_login);
+            this.Name = "Form_login";
+            this.Text = "图书管理系统-客户端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.Button button_register;
+        private System.Windows.Forms.Label label_userName;
+        private System.Windows.Forms.Label label_password;
+        private System.Windows.Forms.TextBox textBox_userName;
+        private System.Windows.Forms.TextBox textBox_password;
     }
 }
 
