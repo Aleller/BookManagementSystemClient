4cc45c695a795ca16dff56bada54bbee546e4d0d
Aleller <1640799791@qq.com>
2018-06-14 18:19:03 +0800

    完成了系统管理员删除图书的功能

--- BookManagementSystemClient/SystemAdministratorChildWindow/Form_systemAdministratorDeleteBook.cs
+++ BookManagementSystemClient/SystemAdministratorChildWindow/Form_systemAdministratorDeleteBook.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookManagementSystemClient.Util;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,6 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Windows.Forms;
 
 namespace BookManagementSystemClient.SystemAdministratorChildWindow
@@ -15,6 +17,43 @@ namespace BookManagementSystemClient.SystemAdministratorChildWindow
         public Form_systemAdministratorDeleteBook()
         {
             InitializeComponent();
+        }
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            bool successful = this.DeleteBook();
+            if (successful)
+            {
+                MessageBox.Show("删除成功");
+            }else
+            {
+                MessageBox.Show("删除失败");
+            }
+        }
+
+        private bool DeleteBook()
+        {
+            string marcNo = textBox_MarcNo.Text.Trim();
+            string url = "http://45.77.191.48:7575/admin/book/delete";
+
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("marcNo", marcNo);
+
+            HttpHandler httpHandler = new HttpHandler();
+
+            string returnString = httpHandler.HttpPost(url, dic);
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            dynamic content = js.Deserialize<dynamic>(returnString);
+
+            string status = content["status"];
+            if(status == "successful")
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
         }
     }
 }
